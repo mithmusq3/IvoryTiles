56f72c1fe6b0163de189fee8e74e489502e58333
mithmusq3 <83664204+mithmusq3@users.noreply.github.com>
2021-08-25 12:08:16 +0530

    Update piano.js

--- piano.js
+++ piano.js
@@ -73,4 +73,90 @@ document.getElementById("A4h").addEventListener('click',function(){au=document.g
 
 document.getElementById("Bh").addEventListener('click',function(){au=document.getElementById('auBh');auM.pause();auM.currentTime=0;au.currentTime=0;au.play();});
 
-document.getElementById("trialbyMithu").addEventListener('click',function(){auM.currentTime=0;auM.play();});
+// document.getElementById("trialbyMithu").addEventListener('click',function(){auM.currentTime=0;auM.play();});
+
+
+something=document.getElementById('auMithu');  
+volumeslider = document.getElementById("volumeslider");
+volumeslider.addEventListener('mousemove',function(){
+    // document.getElementById('auMithu').volume = volumeslider.value/100;
+    document.getElementById('auCb').volume = volumeslider.value/100;
+    document.getElementById('auC4b').volume = volumeslider.value/100;
+    document.getElementById('auDb').volume = volumeslider.value/100;
+    document.getElementById('auD4b').volume = volumeslider.value/100;
+    document.getElementById('auEb').volume = volumeslider.value/100;
+    document.getElementById('auFb').volume = volumeslider.value/100;
+    document.getElementById('auF4b').volume = volumeslider.value/100;
+    document.getElementById('auGb').volume = volumeslider.value/100;
+    document.getElementById('auG4b').volume = volumeslider.value/100;
+    document.getElementById('auAb').volume = volumeslider.value/100;
+    document.getElementById('auA4b').volume = volumeslider.value/100;
+    document.getElementById('auBb').volume = volumeslider.value/100;
+    document.getElementById('auC').volume = volumeslider.value/100;
+    document.getElementById('auC4').volume = volumeslider.value/100;
+    document.getElementById('auD').volume = volumeslider.value/100;
+    document.getElementById('auD4').volume = volumeslider.value/100;
+    document.getElementById('auE').volume = volumeslider.value/100;
+    document.getElementById('auF').volume = volumeslider.value/100;
+    document.getElementById('auF4').volume = volumeslider.value/100;
+    document.getElementById('auG').volume = volumeslider.value/100;
+    document.getElementById('auG4').volume = volumeslider.value/100;
+    document.getElementById('auA').volume = volumeslider.value/100;
+    document.getElementById('auA4').volume = volumeslider.value/100;
+    document.getElementById('auB').volume = volumeslider.value/100;
+    document.getElementById('auCh').volume = volumeslider.value/100;
+    document.getElementById('auC4h').volume = volumeslider.value/100;
+    document.getElementById('auDh').volume = volumeslider.value/100;
+    document.getElementById('auD4h').volume = volumeslider.value/100;
+    document.getElementById('auEh').volume = volumeslider.value/100;
+    document.getElementById('auFh').volume = volumeslider.value/100;
+    document.getElementById('auF4h').volume = volumeslider.value/100;
+    document.getElementById('auGh').volume = volumeslider.value/100;
+    document.getElementById('auG4h').volume = volumeslider.value/100;
+    document.getElementById('auAh').volume = volumeslider.value/100;
+    document.getElementById('auA4h').volume = volumeslider.value/100;
+    document.getElementById('auBh').volume = volumeslider.value/100;
+
+})
+
+	  
+window.addEventListener('keydown', function (e) {
+    key = e.keyCode;
+    if(key==65){
+    	e.preventDefault();
+        au=document.getElementById('auC');auM.pause();auM.currentTime=0;au.currentTime=0;au.play();
+    }
+    if(key==83){
+    	e.preventDefault();
+        au=document.getElementById('auD');auM.pause();auM.currentTime=0;au.currentTime=0;au.play();
+    
+    }
+    if(key==68){
+    	e.preventDefault();
+        au=document.getElementById('auE');auM.pause();auM.currentTime=0;au.currentTime=0;au.play();
+    	
+    }
+    if(key==70){
+    	e.preventDefault();
+        au=document.getElementById('auF');auM.pause();auM.currentTime=0;au.currentTime=0;au.play();
+    
+    }
+	if(key==71){
+		e.preventDefault();
+        au=document.getElementById('auG');auM.pause();auM.currentTime=0;au.currentTime=0;au.play();
+		
+	}
+    if(key==72){
+    	e.preventDefault();
+        au=document.getElementById('auA');auM.pause();auM.currentTime=0;au.currentTime=0;au.play();
+    
+    }
+    if(key==74){
+    	e.preventDefault();
+        au=document.getElementById('auB');auM.pause();auM.currentTime=0;au.currentTime=0;au.play();
+    
+    }
+    
+});
+
+// document.activeElement.blur();
